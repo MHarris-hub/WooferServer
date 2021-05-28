2c35594bca91fb8a14a7083fb9e1930df1f67e97
Michael <michael.jim.harris@gmail.com>
2021-05-28 16:37:33 -0400

    saved changed to WooferConfig, CommentController, UserController

--- src/main/java/com/app/woofer/controller/UserController.java
+++ src/main/java/com/app/woofer/controller/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import com.app.woofer.model.User;
 import com.app.woofer.service.UserService;
 
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://jenkins-testhfsga.s3-website-us-east-1.amazonaws.com")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://jenkins-testhfsga.s3-website-us-east-1.amazonaws.com")
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -60,6 +60,10 @@ public class UserController {
 
     @PostMapping("/login")
     public User login(@RequestBody User user){
+//        if (!userService.login(user)) {
+//            throw new WooferException("Invalid username/password");
+//        }
+
         return userService.login(user);
     }
 
